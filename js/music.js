18309bf63321b73f99065539971eb39dfd7d91b9
aliyahya <68123967+dhafinmaulana@users.noreply.github.com>
2026-02-07 04:00:55 +0700

    Update music.js

--- js/music.js
+++ js/music.js
@@ -2,7 +2,7 @@ const music = document.getElementById("bg-music");
 const toggle = document.getElementById("musicToggle");
 
 if (localStorage.getItem("musicAllowed") === "true") {
-  music.src = "music/lagu2.mp3";
+  music.src = "./music/lagu2.mp3";
   music.volume = 0.6;
   music.play().catch(() => {});
 }
